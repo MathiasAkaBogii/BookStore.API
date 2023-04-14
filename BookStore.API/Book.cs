079d7916e1c15949c62e3f32105710edefad76a1
Mathias Bogner <20182004@htl-neufelden.at>
2023-04-14 10:32:53 +0200

    Add project files.

---
+++ BookStore.API/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.API
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ISBN { get; set; } = string.Empty;
+        public int Pages { get; set; } = 0;
+        public double Rating { get; set; } = 0;
+    }
+}
